3b6b24174a771f2e71ec171707ed3fd1257a6bc5
lheme <lheme@outlook.com.br>
2019-05-23 22:09:23 -0300

    Alterada a implementacao para obert o primeiro paciente e calcular seu IMC somente no caso de peso e altura válidos. Por isso, implementada as validações

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,36 @@
-console.log("Eita, me apartaram do HTML");
-
 var titulo = document.querySelector(".titulo");
 
-titulo.textContent = "Malacrida Nutrição";
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso >= 1000) {
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!";
+}
+
+if (altura <= 0 || altura >= 3.00) {
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida!";
+}
+
+if (pesoEhValido && alturaEhValida) {
+    var imc = (peso / (altura * altura));
+    tdImc.textContent = imc;
+} else {
+    tdImc.textContent = "Altura e/ou peso inválidos!";
+}
+
